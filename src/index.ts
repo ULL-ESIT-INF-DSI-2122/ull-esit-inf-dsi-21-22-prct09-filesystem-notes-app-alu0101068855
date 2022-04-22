6418269cc8c73f58c228c2a0dfeb8fe435ea5f26
Hector Abreu <alu0101068855@ull.edu.es>
2022-04-22 11:15:10 +0000

    Creando método add

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,148 @@
-console.log('Do something');
+import * as fs from 'fs';
+import * as chalk from 'chalk';
+import * as yargs from 'yargs';
+import {spawn} from 'child_process';
+import * as path from 'path';
+
+/*Leer archivo si se encuentra
+fs.readFile('./what.txt', (err, data) => {
+  if(err){
+    console.error('There must be a problem with the file')
+  }
+  else {
+  console.log(data.toString());
+  }
+})*/
+
+/*
+fs.watchFile('helloworld.txt', (curr, prev) => {
+  console.log(`File was ${prev.size} bytes before it was modified.`);
+  console.log(`Now file is ${curr.size} bytes.`);
+
+  // Hacer un cat como bash en la salida estandar del proceso
+  const cat = spawn('cat', ['-n', 'helloworld.txt']);
+  cat.stdout.pipe(process.stdout);
+
+  const wc = spawn('wc', ['helloworld.txt']);
+
+  let wcOutput = '';
+  wc.stdout.on('data', (piece) => wcOutput += piece);
+
+  wc.on('close', () => {
+    const wcOutputAsArray = wcOutput.split(/\s+/);
+    console.log(`File helloworld.txt has ${wcOutputAsArray[1]} lines`);
+    console.log(`File helloworld.txt has ${wcOutputAsArray[2]} words`);
+    console.log(`File helloworld.txt has ${wcOutputAsArray[3]} characters`);
+  });
+});
+
+const inputStream = fs.createReadStream('helloworld.txt');
+
+inputStream.on('data', (piece) => {
+  process.stdout.write(piece);
+});
+
+inputStream.on('error', (err) => {
+  process.stderr.write(err.message);
+});
+*/
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    user: {
+      describe: 'Name user',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Body of the note',
+      demandOption: true,
+      type: 'string',
+    },
+    color: {
+      describe: 'Color of the letters',
+      demandOption: true,
+      type: 'string',
+    }
+  },
+  handler(argv) {
+    if (typeof argv.title === 'string') {
+      console.log(chalk.green('New note added!'));  
+    }
+
+    if (fs.existsSync(`./dist/${argv.user}`)){
+      console.log(chalk.green(`Dir ./dist/${argv.user} exists`));
+    }
+    else{
+      fs.mkdir(path.join(__dirname, String(argv.user)), (err) => {
+        if (err) {
+          return console.error(chalk.red(err));
+        }
+        else {
+          console.log(chalk.green(`Created dir ${argv.user} at ./dist/`));
+        }
+      });
+    }
+
+    //Sin timeout no se puede crear
+    if (fs.existsSync(`./dist/${argv.user}/${argv.title}.txt`)){
+      console.error(chalk.red('This file already exists'));
+    }
+    else{
+      setTimeout(() => {
+        fs.writeFile(`./dist/${argv.user}/${argv.title}.txt`, `${argv.body}`, (err) => {
+          if (err) {
+            return console.error(chalk.red(err));
+          }
+          else{
+            console.log(chalk.green(`Created ${argv.title}.txt`));
+          }
+        });
+      }, 1000);
+    }
+    /*
+    if (argv.color === 'red'){
+      console.log(chalk.red(argv.body));
+    }
+    if (argv.color === 'green'){
+      console.log(chalk.green(argv.body));
+    }
+    if (argv.color === 'blue'){
+      console.log(chalk.blue(argv.body));
+    }
+    if (argv.color === 'yellow'){
+      console.log(chalk.yellow(argv.body));
+    }
+    */
+},
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'Lists all users notes',
+  builder: {
+    user: {
+      describe: 'Username',
+      demandOption: true,
+      type: 'string',
+    } 
+  },
+  handler(argv) {
+    if (fs.existsSync(`./dist/${argv.user}`)){
+      console.log(chalk.green(`Your notes`));
+      let ls = spawn('ls', [`./dist/${argv.user}`]);
+      ls.stdout.pipe(process.stdout);
+    }
+    else{
+      console.error(chalk.red(`Invalid user: ${argv.user}`));
+    }
+  }
+});
+
+yargs.parse();
